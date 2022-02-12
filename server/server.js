bb982668baecddae55816ba61ccb89ff7e48e96c
KangerDrew <kangdrew95@gmail.com>
2022-02-11 19:41:21 -0800

    Deployment test

--- server/server.js
+++ server/server.js
@@ -31,6 +31,10 @@ app.use('/api/applications', applicationsApiRoutes(db));
 const signupApiRoutes = require('./routes/signup');
 app.use('/api/signup', signupApiRoutes(db));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/src'))
+}
+
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
